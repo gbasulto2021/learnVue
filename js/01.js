c88aee23a10a2773ee2ee8f3a37feb475679bb88
George <gbasulto2015@gmail.com>
2021-04-05 14:00:24 -0400

    First commit

---
+++ js/01.js
@@ -0,0 +1,73 @@
+const app = new Vue({
+    el: '#app',
+    data:{
+        titulo: 'Hola mundo desde Vue',
+        frutas: [
+            {nombre: 'Pera', cantidad:10},
+            {nombre: 'Manzana', cantidad:0},
+            {nombre: 'Platano', cantidad:11}
+            
+        ],
+        personas: [
+            {nombre: 'George', edad: 40},
+            {nombre: 'Luis', edad: 68},
+            {nombre: 'Bry', edad: 11},
+            {nombre: 'Luis Manuel', edad: 12}
+        ],
+        nuevaFruta: '',
+        cantidadFrutas: '',
+        nuevaPersona:'',
+        edadPersona:'',
+        total: 0
+    },
+    methods:{
+      agregarFruta (){
+          this.frutas.push(
+              {nombre: this.nuevaFruta, cantidad: this.cantidadFrutas}
+              
+              );
+          this.nuevaFruta= ''
+          this.cantidadFrutas=''
+      },
+
+      agregarPersona (){
+          this.personas.push(
+              {nombre: this.nuevaPersona, edad:this.edadPersona}
+          );
+          this.nuevaPersona = ''
+          this.edadPersona = ''
+
+      }
+    },
+    computed:{
+      sumarFrutas (){
+          this.total = 0;
+          for (fruta of this.frutas){
+              this.total = this.total + fruta.cantidad;
+          } 
+          return this.total;
+      }
+    }
+});
+
+const app2 = new Vue({
+    el: '#app2',
+    data: {
+      fondo: 'bg-warning',
+      color:true
+    },
+    methods:{
+
+    }
+});
+
+const app3 = new Vue({
+    el: '#app3',
+    data:{
+        titulo: "Gym con Vue"
+
+    },
+    methods: {
+
+    }
+});
